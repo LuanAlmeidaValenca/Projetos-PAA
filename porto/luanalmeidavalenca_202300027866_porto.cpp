6c74efec2c163f50dff8eee6911c42a6a2d2b5fc
LuanAlmeidaValenca <luan.valenca@hotmail.com>
2025-11-03 09:57:27 -0300

    Update luanalmeidavalenca_202300027866_porto.cpp

--- porto/luanalmeidavalenca_202300027866_porto.cpp
+++ porto/luanalmeidavalenca_202300027866_porto.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <unordered_map>
-#include <iomanip>
 #include <cmath>
-
 using namespace std;
 
 struct Container {
@@ -16,149 +12,139 @@ struct Container {
 
 struct Divergencia {
     string codigo;
-    string cnpj1;
-    string cnpj2;
-    int diffPeso;
-    int perc;
-    bool divergenciaCNPJ;
-    int ordem;
+    string info;
+    int prioridade;
+    double diff;
 };
 
-// Função de leitura de arquivo
-bool lerArquivo(
-    const string& nomeArquivo,
-    vector<Container>& cadastro,                    // mantém a ordem
-    unordered_map<string, Container>& inspecionados // busca rápida
-) {
-    ifstream arquivo(nomeArquivo);
-    if (!arquivo) {
-        cerr << "Erro ao abrir o arquivo: " << nomeArquivo << "\n";
-        return false;
-    }
+struct HashItem {
+    string codigo;
+    string cnpj;
+    int peso;
+    bool ocupado;
+};
 
-    int n1, n2;
-    if (!(arquivo >> n1)) return false;
-
-    cadastro.reserve(n1);
-    for (int i = 0; i < n1; i++) {
-        Container r;
-        arquivo >> r.codigo >> r.cnpj >> r.peso;
-        cadastro.push_back(r);
-    }
-
-    if (!(arquivo >> n2)) return false;
-
-    for (int i = 0; i < n2; i++) {
-        Container r;
-        arquivo >> r.codigo >> r.cnpj >> r.peso;
-        inspecionados[r.codigo] = r;
-    }
-
-    arquivo.close();
-    return true;
+int hashFunc(const string& codigo, int tamanho) {
+    unsigned long h = 5381;
+    for (char c : codigo)
+        h = ((h << 5) + h) + c;
+    return h % tamanho;
 }
 
-// Função MergeSort (estável)
-void merge(vector<Divergencia>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    vector<Divergencia> L(n1), R(n2);
-    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        bool cond = false;
-
-        // Ordena primeiro por divergência de CNPJ
-        if (L[i].divergenciaCNPJ != R[j].divergenciaCNPJ)
-            cond = L[i].divergenciaCNPJ > R[j].divergenciaCNPJ;
-        // Depois pela diferença percentual
-        else if (L[i].perc != R[j].perc)
-            cond = L[i].perc > R[j].perc;
-        // Por último, pela ordem original
-        else
-            cond = L[i].ordem < R[j].ordem;
-
-        if (cond)
-            arr[k++] = L[i++];
-        else
-            arr[k++] = R[j++];
+void inserirHash(HashItem* tabela, int tamanho, const Container& c) {
+    int idx = hashFunc(c.codigo, tamanho);
+    while (tabela[idx].ocupado) {
+        idx = (idx + 1) % tamanho;
     }
+    tabela[idx].codigo = c.codigo;
+    tabela[idx].cnpj = c.cnpj;
+    tabela[idx].peso = c.peso;
+    tabela[idx].ocupado = true;
+}
 
+bool buscarHash(HashItem* tabela, int tamanho, const string& codigo, Container& resultado) {
+    int idx = hashFunc(codigo, tamanho);
+    int inicio = idx;
+    while (tabela[idx].ocupado) {
+        if (tabela[idx].codigo == codigo) {
+            resultado.codigo = tabela[idx].codigo;
+            resultado.cnpj = tabela[idx].cnpj;
+            resultado.peso = tabela[idx].peso;
+            return true;
+        }
+        idx = (idx + 1) % tamanho;
+        if (idx == inicio) break;
+    }
+    return false;
+}
+
+void merge(Divergencia arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    Divergencia* L = new Divergencia[n1];
+    Divergencia* R = new Divergencia[n2];
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        bool menor = false;
+        if (L[i].prioridade < R[j].prioridade) menor = true;
+        else if (L[i].prioridade == R[j].prioridade && L[i].diff > R[j].diff) menor = true;
+        if (menor) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
     while (i < n1) arr[k++] = L[i++];
     while (j < n2) arr[k++] = R[j++];
+    delete[] L;
+    delete[] R;
 }
 
-void mergeSort(vector<Divergencia>& arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+void mergeSort(Divergencia arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
-// Função principal
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        cerr << "Uso: " << argv[0] << " <entrada.txt> <saida.txt>\n";
-        return 1;
-    }
+    ifstream input(argv[1]);
+    ofstream output(argv[2]);
+    if (!input || !output) return 1;
 
-    vector<Container> cadastro;
-    unordered_map<string, Container> inspecionados;
+    int n1, n2;
+    input >> n1;
+    Container* cadastro = new Container[n1];
+    for (int i = 0; i < n1; i++)
+        input >> cadastro[i].codigo >> cadastro[i].cnpj >> cadastro[i].peso;
 
-    if (!lerArquivo(argv[1], cadastro, inspecionados)) {
-        cerr << "Erro na leitura do arquivo.\n";
-        return 1;
-    }
+    input >> n2;
+    Container* inspecionados = new Container[n2];
+    for (int i = 0; i < n2; i++)
+        input >> inspecionados[i].codigo >> inspecionados[i].cnpj >> inspecionados[i].peso;
 
-    vector<Divergencia> divergencias;
-    divergencias.reserve(cadastro.size());
+    int tamanhoHash = n2 * 2 + 1;
+    HashItem* tabela = new HashItem[tamanhoHash];
+    for (int i = 0; i < tamanhoHash; i++) tabela[i].ocupado = false;
+    for (int i = 0; i < n2; i++) inserirHash(tabela, tamanhoHash, inspecionados[i]);
 
-    for (size_t i = 0; i < cadastro.size(); ++i) {
-        const auto& c1 = cadastro[i];
-        if (inspecionados.find(c1.codigo) == inspecionados.end())
-            continue;
+    Divergencia* diverg = new Divergencia[n1];
+    int count = 0;
 
-        const auto& c2 = inspecionados[c1.codigo];
-
-        bool cnpjDif = (c1.cnpj != c2.cnpj);
-        int diff = abs(c2.peso - c1.peso);
-        int perc = (int)round((diff * 100.0) / c1.peso);
-
-        if (cnpjDif || perc > 10) {
-            Divergencia d;
-            d.codigo = c1.codigo;
-            d.cnpj1 = c1.cnpj;
-            d.cnpj2 = c2.cnpj;
-            d.diffPeso = diff;
-            d.perc = perc;
-            d.divergenciaCNPJ = cnpjDif;
-            d.ordem = (int)i;
-            divergencias.push_back(d);
+    for (int i = 0; i < n1; i++) {
+        Container achado;
+        if (buscarHash(tabela, tamanhoHash, cadastro[i].codigo, achado)) {
+            if (cadastro[i].cnpj != achado.cnpj) {
+                diverg[count].codigo = cadastro[i].codigo;
+                diverg[count].info = cadastro[i].cnpj + "<->" + achado.cnpj;
+                diverg[count].prioridade = 1;
+                diverg[count].diff = 0;
+                count++;
+            } else {
+                double diff = fabs(cadastro[i].peso - achado.peso);
+                double perc = (diff / cadastro[i].peso) * 100.0;
+                if (perc > 10.0) {
+                    diverg[count].codigo = cadastro[i].codigo;
+                    diverg[count].info = to_string((int)diff) + "kg(" + to_string((int)round(perc)) + "%)";
+                    diverg[count].prioridade = 2;
+                    diverg[count].diff = perc;
+                    count++;
+                }
+            }
         }
     }
 
-    // Ordena divergências
-    mergeSort(divergencias, 0, (int)divergencias.size() - 1);
+    mergeSort(diverg, 0, count - 1);
 
-    // Escreve saída
-    ofstream output(argv[2]);
-    if (!output) {
-        cerr << "Erro ao criar arquivo de saída.\n";
-        return 1;
-    }
+    for (int i = 0; i < count; i++)
+        output << diverg[i].codigo << ":" << diverg[i].info << "\n";
 
-    for (const auto& d : divergencias) {
-        if (d.divergenciaCNPJ)
-            output << d.codigo << ":" << d.cnpj1 << "<->" << d.cnpj2 << "\n";
-        else
-            output << d.codigo << ":" << d.diffPeso << "kg(" << d.perc << "%)\n";
-    }
-
+    delete[] cadastro;
+    delete[] inspecionados;
+    delete[] diverg;
+    delete[] tabela;
+    input.close();
     output.close();
     return 0;
 }
